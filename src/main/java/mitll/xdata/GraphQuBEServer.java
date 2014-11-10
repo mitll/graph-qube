4efcb93585f4400e22304e3d5a40850f6993827b
Charlie Dagli <dagli@ll.mit.edu>
2014-11-10 10:59:22 -0500

    minor change to license

--- src/main/java/mitll/xdata/GraphQuBEServer.java
+++ src/main/java/mitll/xdata/GraphQuBEServer.java
@@ -1,4 +1,4 @@
-// Copyright 2013 MIT Lincoln Laboratory, Massachusetts Institute of Technology 
+// Copyright 2014 MIT Lincoln Laboratory, Massachusetts Institute of Technology 
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
